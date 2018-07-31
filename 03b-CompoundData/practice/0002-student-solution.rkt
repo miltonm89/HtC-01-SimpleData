9ab3c4494c68a195c2bc23ee7899c8df7caf2eea
Milto007 <Milton.morales@gmail.com>
2018-07-31 10:45:53 -0500

    Completed 03b pracitce problem 2
    Completed the suggested student practice problem for 03b

--- 03b-CompoundData/practice/0002-student-solution.rkt
+++ 03b-CompoundData/practice/0002-student-solution.rkt
@@ -16,6 +16,27 @@
 ;; On the trip, lunch must be provided for all students. For each student, track 
 ;; their name, their grade (from 1 to 12), and whether or not they have allergies.
 
+(define-struct student (name lvl allergy))
+;; Student is (make-student String Natural Bool)
+;; interp. (make-student name lvl allergy) is a student with
+;; - name is the student's name
+;; - lvl is the student's grade level
+;; - allergy is the student's allergic state
+(define S1 (make-student "Milton" 9 true))
+(define S2 (make-student "Rebecca" 10 false))
+(define S3 (make-student "Daisy" 3 true))
+(define S4 (make-student "Jaime" 4 false))
+
+#;
+(define (fn-for-student s)
+  (... (student-name s)      ; String
+       (student-lvl s)       ; Natural
+       (student-allergy s))) ; Bool
+
+;; Template rules used:
+;; - compound data: 3 fields
+
+
 ;; =========================
 ;; Functions:
 ;;
@@ -24,5 +45,22 @@
 ;; is responsible for keeping track of their allergies. If a student has allergies, 
 ;; and is in a qualifying grade, their name should be added to a special list. 
 ;; Design a function to produce true if a student name should be added to this list.
+
+;; qualify-allergies
+;; Student -> Bool
+;; Input a student and check if student is in grade 6 or below and has allergies
+;; Output true if both conditions are met, otherwise false
+(check-expect (qualify-allergies S1) false)
+(check-expect (qualify-allergies S2) false)
+(check-expect (qualify-allergies S3) true)
+(check-expect (qualify-allergies S4) false)
+
+; (define (qualify-allergies s) false) ; stub
+
+(define (qualify-allergies s)
+  (and (<= (student-lvl s) 6) 
+       (student-allergy s)))
+
+;; <Template from Student>
 
 (test)

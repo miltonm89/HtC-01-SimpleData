1d8418c3ed71bde9cb3da7582c0687952adaea9f
Milton Morales <Milton.morales@gmail.com>
2023-11-07 09:59:20 -0600

    Last update, never commited I guess

--- 04a-SelfReference/lectures/003-quidditch-recap-starter.rkt
+++ 04a-SelfReference/lectures/003-quidditch-recap-starter.rkt
@@ -8,53 +8,51 @@
 ;(check-expect false true)
 ;(check-expect true false)
 
+;; quidditch-recap-starter.rkt
 
 ;;PROBLEM:
 
 ;;Imagine that you are designing a program that will keep track of
 ;;your favorite Quidditch teams. (http://iqasport.org/).
 
-;;Design a data definition to represent a list of Quidditch teams.
+;;(A) Design a data definition to represent a list of Quidditch teams.
+;;(B) Design a function that consumes ListOfString and produces true if 
+;;    the list includes "UBC".
 
-;; ListOfString is one of:
-;; - empty
-;; - (cons String ListOfString)
+;; ListOfString is one of: 
+;;  - empty
+;;  - (cons String ListOfString)
 ;; interp. a list of strings
 (define LOS1 empty)
 (define LOS2 (cons "McGill" empty))
 (define LOS3 (cons "UBC" (cons "McGill" empty)))
-
 #;
 (define (fn-for-los los)
-    (cond [(empty? los) (...)]
-          [else
-           (... (first los)
-                (rest los))]))
+  (cond [(empty? los) (...)]
+        [else
+         (... (first los)     
+              (fn-for-los (rest los)))]))
 
 ;; Template rules used:
-;; - one of: 2 cases
-
-
-;;PROBLEM:
-
-;;We want to know whether your list of favorite Quidditch teams includes
-;;UBC! Design a function that consumes ListOfString and produces true if 
-;;the list includes "UBC".
+;;  - one of: 2 cases
+;;  - atomic distinct: empty
+;;  - compound: (cons String ListOfString)
+;;  - self-reference: (rest los) is ListOfString
 
 ;; ListOfString -> Boolean
-;; prodce true if los includes "UBC"
-
+;; produce true if los includes "UBC"
 (check-expect (contains-ubc? empty) false)
 (check-expect (contains-ubc? (cons "McGill" empty)) false)
 (check-expect (contains-ubc? (cons "UBC" empty)) true)
 (check-expect (contains-ubc? (cons "McGill" (cons "UBC" empty))) true)
 
-;(define (constains-ubc? los) false) ; stub
+;(define (contains-ubc? los) false) ;stub
+
 (define (contains-ubc? los)
-    (cond [(empty? los) false]
-          [else
-           (if (string=? (first los) "UBC")
-               true
-                (contains-ubc? (rest los)))]))
+  (cond [(empty? los) false]
+        [else
+         (if (string=? (first los) "UBC")
+             true
+              (contains-ubc? (rest los)))]))
 
 (test)
